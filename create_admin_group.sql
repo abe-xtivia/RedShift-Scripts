a3512d79021712ed11216bfa437cc016834c5ad2
abe-xtivia <asmith@xtivia.com>
2018-02-07 12:12:38 -0700

    Initial

--- create_admin_group.sql
+++ create_admin_group.sql
@@ -4,9 +4,11 @@ CREATE GROUP admins;
 -- Grants usage to group on specific schema
 GRANT USAGE on SCHEMA [schemaname] to GROUP admins;
 
--- Give read-only access of particular schema to group
-GRANT SELECT, UPDATE, INSERT, DELETE ON ALL TABLES IN SCHEMA [schemaname] to GROUP admins;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA [schemaname] to GROUP admins;
+-- Grants Create on database for admin group (enables creation of schemas)
+GRANT CREATE on DATABASE [database] TO GROUP admins;
+
+-- Give all access of particular schema to group
+GRANT ALL ON SCHEMA [schemaname] to GROUP admins;
 
 --Modify default schema privileges
 --Use this initially if the default privileges have not been set for new objects for groups
